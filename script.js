0b60384498586ca75929726f906a56b6030618b2
HiromiRillz <hiromirilloraza.t@gmail.com>
2023-09-18 22:09:02 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists)
+
+const pullDownButton = document.getElementById("lists")
+console.log(pullDownButton)
+
+window.addEventListener('click', function(){
+    const pullDownButton = document.getElementById("lists")
+    console.log(pullDownButton)
+})
+window.addEventListener('mouseout', function(){
+    const pullDownButton = document.getElementById("lists")
+    console.log(pullDownButton)
+})
