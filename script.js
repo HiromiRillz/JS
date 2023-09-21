da062d629efdb8686fb900f87c19fbfc59794f59
HiromiRillz <hiromirilloraza.t@gmail.com>
2023-09-21 15:05:21 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
 
-window.addEventListener('mouseover', function(){
-     const pullDownButton = document.getElementById("lists")
-     console.log("乗る")
- })
- window.addEventListener('mouseout', function(){
-    const pullDownButton = document.getElementById("lists")
-    console.log("外れる")
-})
-window.addEventListener('click', function(){
-    const pullDownButton = document.getElementById("lists")
-    console.log("クリック")
-})
 pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style","background-color:blue;")
-})
+     const pullDownButton = document.getElementById("lists")
+     this.setAttribute("style","background-color:blue;")
+     console.log("乗ったら青")
+ })
 pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style","background-color:red;")
+    const pullDownButton = document.getElementById("lists")
+    this.removeAttribute("style","background-color:red;")
+    console.log("外れたら赤")
 })
 pullDownButton.addEventListener('click', function(){
-    pullDownButton.setAttribute("style","background-color:green;")
+    const pullDownButton = document.getElementById("lists")
+    this.setAttribute("style","background-color:green;")
+    console.log("クリックしたら緑")
+})
+
+const pullDownParents = document.getElementById("pull-down")
+pullDownButton.addEventListener('click', function(){
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style", "display:block;")
+        console.log("表示")
+    }
 })
