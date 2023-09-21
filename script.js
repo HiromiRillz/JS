dc96926af0787ab8a3f897b568fc6c05bc4be2be
HiromiRillz <hiromirilloraza.t@gmail.com>
2023-09-21 14:36:06 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
-
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
 
+window.addEventListener('mouseover', function(){
+     const pullDownButton = document.getElementById("lists")
+     console.log("乗る")
+ })
+ window.addEventListener('mouseout', function(){
+    const pullDownButton = document.getElementById("lists")
+    console.log("外れる")
+})
 window.addEventListener('click', function(){
     const pullDownButton = document.getElementById("lists")
-    console.log(pullDownButton)
+    console.log("クリック")
 })
-window.addEventListener('mouseout', function(){
-    const pullDownButton = document.getElementById("lists")
-    console.log(pullDownButton)
+pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style","background-color:blue;")
+})
+pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style","background-color:red;")
+})
+pullDownButton.addEventListener('click', function(){
+    pullDownButton.setAttribute("style","background-color:green;")
 })
